d1c21aedb7a276d813d0f6f798ebe91b29bdeefa
cjcarroll <cjcarrollswim@gmail.com>
2015-04-06 13:56:28 -0500

    Finished backend

--- src/BackEnd.java
+++ src/BackEnd.java
@@ -1,21 +1,39 @@
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class BackEnd {
 
+	private List<String> users;
+	
+	public BackEnd()
+	{
+		users = new ArrayList<String>();
+	}
 	public List<String> getMessages(String username)
 	{
-		return null;
+		Encoder enc = new Encoder(username + ".txt");
+		return enc.read();
 	}
 	
 	public void addMessage(String username, String msg)
 	{
-		
+		boolean contains = false;
+		Encoder enc = new Encoder(username + ".txt");
+		for (int i = 0; i < users.size(); i++)
+			if (username.equals(users.get(i)))
+			{
+				enc.write(msg);
+				contains = true;
+			}
+		if (contains == false)
+			users.add(username);
+			
 	}
 	
 	public List<String> getUsers()
 	{
-		return null;
+		return users;
 	}
 	
 	
