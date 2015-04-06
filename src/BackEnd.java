9e95f1bfa8e7a35c16be4029b6789ac0dfb3d39d
cjcarroll <cjcarrollswim@gmail.com>
2015-04-06 12:54:51 -0500

    BackEnd

---
+++ src/BackEnd.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+
+public class BackEnd {
+
+	public List<String> getMessages(String username)
+	{
+		return null;
+	}
+	
+	public void addMessage(String username, String msg)
+	{
+		
+	}
+	
+	public List<String> getUsers()
+	{
+		return null;
+	}
+	
+	
+	
+	
+	
+	
+}
